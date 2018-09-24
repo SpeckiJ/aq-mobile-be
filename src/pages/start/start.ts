36dadd4b87cc32241bee42216f725565439c49a7
Jan Schulte <j.schulte@52north.org>
2018-09-24 16:05:13 +0200

    first test

--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -4,6 +4,8 @@ import { NavController } from 'ionic-angular';
 import { LocateProvider } from '../../providers/locate/locate';
 import { UserLocationListProvider } from '../../providers/user-location-list/user-location-list';
 import { MapPage } from '../map/map';
+import { HeaderContent } from '../../components/belaqi-user-location-slider/belaqi-user-location-slider';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'page-start',
@@ -15,10 +17,13 @@ export class StartPage {
   public locationCount: number;
   public name = 'start';
 
+  public sliderHeaderContent: HeaderContent;
+
   constructor(
     private nav: NavController,
     private locate: LocateProvider,
-    private userLocations: UserLocationListProvider
+    private userLocations: UserLocationListProvider,
+    public translateSrvc: TranslateService
   ) {
     this.locate.getLocationStateEnabled().subscribe(enabled => this.locationEnabled = enabled);
     this.userLocations.getAllLocations().subscribe(list => this.locationCount = list.length);
@@ -26,6 +31,10 @@ export class StartPage {
 
   public navigateToMap(phenomenonId: string) {
     this.nav.push(MapPage, { phenomenonId });
+  }
+
+  public setHeaderContent(headerContent: HeaderContent) {
+    this.sliderHeaderContent = headerContent;
   }
 
 }
